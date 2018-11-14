681845ce3fd631d97da32cbe40f503b07222c971
Takatsugu Nokubi <takatsugu.nokubi@robotfund.co.jp>
2018-11-14 12:58:18 +0900

    execute classifier with cola like data

---
+++ run_cola.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+export BERT_BASE_DIR=`pwd`/multilingual_L-12_H-768_A-12
+export GLUE_DIR=`pwd`
+export PYTHONPATH=`pwd`/bert
+python bert/run_classifier.py \
+  --task_name=cola \
+  --do_train=true \
+  --do_eval=true \
+  --data_dir=$GLUE_DIR \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --max_seq_length=128 \
+  --train_batch_size=32 \
+  --learning_rate=2e-5 \
+  --num_train_epochs=3.0 \
+  --output_dir=cola_output/
