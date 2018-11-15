096e89774ab27747800e339735e265c87013876b
Takatsugu Nokubi <takatsugu.nokubi@robotfund.co.jp>
2018-11-15 17:29:57 +0900

    train using aozora data

---
+++ aozora_cola.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+export BERT_BASE_DIR=`pwd`/multilingual_L-12_H-768_A-12
+export GLUE_DIR=`pwd`/aozora
+export PYTHONPATH=`pwd`/bert
+
+BIN_PYTHON=python
+EPOCH=3
+NO_CHKPT=0
+while getopts e:p:n opts ; do
+    case $opts in
+	e)
+	    EPOCH=$OPTARG
+	    ;;
+	p)
+	    BIN_PYTHON=$OPTARG
+	    ;;
+	n)
+	    NO_CHKPT=1
+	    ;;
+    esac
+done
+OUTPUT_PATH=aozora/cola_${EPOCH}_output
+
+if [ $NO_CHKPT -eq 0 ] ; then
+    $BIN_PYTHON bert/run_classifier.py \
+		--task_name=cola \
+		--do_train=true \
+		--do_eval=true \
+		--data_dir=$GLUE_DIR \
+		--vocab_file=$BERT_BASE_DIR/vocab.txt \
+		--bert_config_file=$BERT_BASE_DIR/bert_config.json \
+		--init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+		--max_seq_length=128 \
+		--train_batch_size=32 \
+		--learning_rate=2e-5 \
+		--num_train_epochs=$EPOCH \
+		--output_dir=$OUTPUT_PATH
+else
+    OUTPUT_PATH=${OUTPUT_PATH}_nockpt
+    $BIN_PYTHON bert/run_classifier.py \
+		--task_name=cola \
+		--do_train=true \
+		--do_eval=true \
+		--data_dir=$GLUE_DIR \
+		--vocab_file=$BERT_BASE_DIR/vocab.txt \
+		--bert_config_file=$BERT_BASE_DIR/bert_config.json \
+		--max_seq_length=128 \
+		--train_batch_size=32 \
+		--learning_rate=2e-5 \
+		--num_train_epochs=$EPOCH \
+		--output_dir=$OUTPUT_PATH
+fi
